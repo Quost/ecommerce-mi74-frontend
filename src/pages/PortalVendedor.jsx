293e61dffdb1abfaaaffbf3f7e6d684ae3be31ff
Pedro-Panstein <phfarof@gmail.com>
2024-10-25 21:38:04 -0300

    nome do arquivo alterado

--- src/pages/PortalVendedor.jsx
+++ src/pages/PortalVendedor.jsx
@@ -1,10 +1,11 @@
-import ProductList from "../components/ProductList";
+import ProductList from "../components/ProdutoVendedor";
+import camisetapreta from "../assets/camiseta-preta.jpeg";
 
 export default function PortalVendedor() {
     return (
         <>
             <div className="bg-bg-whitePersonalized h-screen">
-                <ProductList></ProductList>
+                <ProductList imagem={camisetapreta} valor={70} quantidade={26} nome={"Camiseta preta"} />
             </div>
         </>
     );
