5c200a5d72bc1dfd96393d2856a319c7d1ab19a0
pedro <phfarof@gmail.com>
2024-10-26 00:58:00 -0300

    Mudanças na estilização dos inputs

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
     extend: {
       backgroundColor: {
         'bg-whitePersonalized': "#f5f5f5",
+      },
+      boxShadow: {
+        'box-shadow-personalized': "inset 0px 0px 10px 0 rgb(0 0 0 /0.25)",
+      },
+      borderColor: {
+        'border-rgba-black': "rgba(0, 0, 0, 0.2)",
       }
     },
   },
