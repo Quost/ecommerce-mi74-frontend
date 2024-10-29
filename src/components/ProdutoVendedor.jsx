fe64054cfda0259df509cde94f07fc62b93d4ec6
Pedro-Panstein <phfarof@gmail.com>
2024-10-28 21:47:13 -0300

    Lembrar de arrumar o get de quantidade

--- src/components/ProdutoVendedor.jsx
+++ src/components/ProdutoVendedor.jsx
@@ -1,13 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-export default function Product({
-  imagem,
-  valor,
-  quantidade,
-  nome,
-  setOpen,
-}) {
+export default function Product({imagem, custo, quantidade, nome, setOpen}) {
   return (
     <>
       <div className="bg-[#7389AE] w-[90%] mx-auto h-[150px] rounded-lg shadow-md flex items-center justify-between p-2 transition-all text-white text-xl">
@@ -17,13 +11,13 @@ export default function Product({
           className="h-[90%] rounded-lg ml-2 w-[120px]"
         />
         <p className="">Nome: {nome}</p>
-        <p>R$: {valor}</p>
+        <p>R$: {custo}</p>
         <p>Quantidade: {quantidade}</p>
         <div className="flex gap-10 mr-2">
           <FontAwesomeIcon
             icon={faPen}
             className="text-white hover:scale-110 text-[2rem] cursor-pointer"
-            onClick={() => setOpen(nome, valor, quantidade, imagem)}
+            onClick={() => setOpen(nome, custo, quantidade, imagem)}
           />
           <FontAwesomeIcon
             icon={faTrash}
